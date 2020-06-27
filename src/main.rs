19f6c9b12f5f6432038852eaf6e1edb88c385e9c
Charlie Stanton <charlie@shtanton.com>
2020-06-27 07:46:21 +0100

    Adds text based volume indicator with iced
    Also doesn't actually record now

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,179 @@
 extern crate cpal;
+extern crate futures;
 extern crate hound;
+extern crate iced;
 
 use std::io::{self, BufRead};
-use std::sync::{atomic::AtomicBool, Arc, Mutex};
-use std::time::{Instant};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
+};
+use std::time::{Duration, Instant};
 
-use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
+use cpal::{
+    traits::{DeviceTrait, EventLoopTrait, HostTrait},
+    Sample,
+};
 
+use iced::futures::{
+    channel::mpsc::{channel, Receiver},
+    executor::ThreadPool,
+    future,
+    stream::{BoxStream, StreamExt},
+};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use iced::{executor, Application, Column, Command, Element, Settings, Subscription, Text};
+
+#[derive(Debug)]
+enum Message {
+    Sample(f32),
+}
+
+struct App {
+    pos_volume: usize,
+    neg_volume: usize,
+    host: Arc<cpal::Host>,
+    device: Arc<cpal::Device>,
+    format: Arc<cpal::Format>,
+    recording: Arc<AtomicBool>,
+}
+
+impl Application for App {
+    type Executor = executor::Default;
+    type Message = Message;
+    type Flags = ();
+
+    fn new(_flags: Self::Flags) -> (App, Command<Self::Message>) {
+        let host = cpal::default_host();
+        let device = host
+            .default_input_device()
+            .expect("Failed to get default input device");
+        let format = device
+            .default_input_format()
+            .expect("Failed to get default input format");
+
+        (
+            App {
+                pos_volume: 0,
+                neg_volume: 0,
+                host: Arc::new(host),
+                device: Arc::new(device),
+                format: Arc::new(format),
+                recording: Arc::new(AtomicBool::new(true)),
+            },
+            Command::none(),
+        )
+    }
+
+    fn title(&self) -> String {
+        "Recorder".to_string()
+    }
+
+    fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
+        match message {
+            Message::Sample(sample) => {
+                println!("{}", sample);
+                if sample > 0. {
+                    self.pos_volume = (sample * 20.) as usize;
+                } else if sample < 0. {
+                    self.neg_volume = (-sample * 20.) as usize;
+                } else {
+                    self.pos_volume = 0;
+                    self.neg_volume = 0;
+                }
+                Command::none()
+            }
+        }
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Column::new()
+            .push(Text::new(format!(
+                "Pos volume: {:#<1$}",
+                "", self.pos_volume
+            )))
+            .push(Text::new(format!(
+                "Neg volume: {:#<1$}",
+                "", self.neg_volume
+            )))
+            .into()
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        audio_subscription(
+            self.host.clone(),
+            self.device.clone(),
+            self.format.clone(),
+            self.recording.clone(),
+        )
+        .map(Message::Sample)
+    }
+}
+
+pub fn main() {
+    App::run(Settings::default());
+}
+
+fn audio_subscription(
+    host: Arc<cpal::Host>,
+    device: Arc<cpal::Device>,
+    format: Arc<cpal::Format>,
+    recording: Arc<AtomicBool>,
+) -> iced::Subscription<f32> {
+    iced::Subscription::from_recipe(AudioIn {
+        host,
+        device,
+        format,
+        recording,
+    })
+}
+
+struct AudioIn {
+    host: Arc<cpal::Host>,
+    device: Arc<cpal::Device>,
+    format: Arc<cpal::Format>,
+    recording: Arc<AtomicBool>,
+}
+
+impl<H, I> iced_native::subscription::Recipe<H, I> for AudioIn
+where
+    H: std::hash::Hasher,
+{
+    type Output = f32;
+
+    fn hash(&self, state: &mut H) {
+        use std::hash::Hash;
+        std::any::TypeId::of::<AudioIn>().hash(state);
+    }
+
+    fn stream(self: Box<Self>, _input: BoxStream<'static, I>) -> BoxStream<'static, Self::Output> {
+        Box::pin(
+            record(
+                &self.host,
+                &self.device,
+                &self.format,
+                self.recording.clone(),
+            )
+            .unwrap()
+            .scan((Instant::now(), 0.), |(started, largest), sample| {
+                let so_far = sample.max(*largest);
+                future::ready(if started.elapsed() > Duration::from_millis(33) {
+                    *started += Duration::from_millis(33);
+                    *largest = 0.;
+                    Some((true, so_far))
+                } else {
+                    *largest = so_far;
+                    Some((false, so_far))
+                })
+            })
+            .filter_map(|(important, v)| {
+                future::ready(if important { Some(v.min(1.)) } else { None })
+            }),
+        )
+    }
+}
+
+pub fn do_record() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = std::env::args().collect();
     let path = &args[1];
 
@@ -20,7 +185,26 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .default_input_format()
         .expect("Failed to get default input format");
 
-    let (recording, writer) = record(path, host, device, format)?;
+    let recording = Arc::new(AtomicBool::new(true));
+    let samples = record(&host, &device, &format, recording.clone())?;
+
+    let mut writer = hound::WavWriter::create(path, wav_spec_from_format(&format))?;
+
+    let recording_task = async {
+        samples
+            .for_each(|sample| {
+                writer.write_sample(sample).ok();
+                if sample > 0. {
+                    println!("Sample: {:#<1$}", "", (sample * 20.) as usize);
+                }
+                async {}
+            })
+            .await;
+        writer.finalize().unwrap();
+    };
+
+    let pool = ThreadPool::new().unwrap();
+    pool.spawn_ok(recording_task);
 
     let start_time = Instant::now();
 
@@ -34,36 +218,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         };
         println!("{}", response);
     }
+    recording.store(false, Ordering::Relaxed);
 
-    recording.store(false, std::sync::atomic::Ordering::Relaxed);
-    writer.lock().unwrap().take().unwrap().finalize()?;
     Ok(())
 }
 
 fn record(
-    path: &str,
-    host: cpal::Host,
-    device: cpal::Device,
-    format: cpal::Format,
-) -> Result<
-    (
-        Arc<AtomicBool>,
-        Arc<Mutex<Option<hound::WavWriter<std::io::BufWriter<std::fs::File>>>>>,
-    ),
-    Box<dyn std::error::Error>,
-> {
+    host: &cpal::Host,
+    device: &cpal::Device,
+    format: &cpal::Format,
+    recording: Arc<AtomicBool>,
+) -> Result<Receiver<f32>, Box<dyn std::error::Error>> {
     let event_loop = host.event_loop();
-    let stream_id = event_loop.build_input_stream(&device, &format)?;
+    let stream_id = event_loop.build_input_stream(&device, format)?;
     event_loop.play_stream(stream_id)?;
 
-    let spec = wav_spec_from_format(&format);
-    let writer = hound::WavWriter::create(path, spec)?;
-    let writer = Arc::new(Mutex::new(Some(writer)));
+    let (mut sender, receiver) = channel(1024);
 
-    let recording = Arc::new(AtomicBool::new(true));
-
-    let writer_2 = writer.clone();
-    let recording_2 = recording.clone();
     std::thread::spawn(move || {
         event_loop.run(move |id, event| {
             let data = match event {
@@ -74,51 +245,38 @@ fn record(
                 }
             };
 
-            // If we're done recording, return early.
-            if !recording_2.load(std::sync::atomic::Ordering::Relaxed) {
+            if !recording.load(Ordering::Relaxed) {
+                sender.close_channel();
                 return;
             }
-            // Otherwise write to the wav writer.
+
             match data {
                 cpal::StreamData::Input {
                     buffer: cpal::UnknownTypeInputBuffer::U16(buffer),
                 } => {
-                    if let Ok(mut guard) = writer_2.try_lock() {
-                        if let Some(writer) = guard.as_mut() {
-                            for sample in buffer.iter() {
-                                let sample = cpal::Sample::to_i16(sample);
-                                writer.write_sample(sample).ok();
-                            }
-                        }
+                    for sample in buffer.iter() {
+                        sender.try_send(sample.to_f32()).unwrap();
                     }
                 }
                 cpal::StreamData::Input {
                     buffer: cpal::UnknownTypeInputBuffer::I16(buffer),
                 } => {
-                    if let Ok(mut guard) = writer_2.try_lock() {
-                        if let Some(writer) = guard.as_mut() {
-                            for &sample in buffer.iter() {
-                                writer.write_sample(sample).ok();
-                            }
-                        }
+                    for &sample in buffer.iter() {
+                        sender.try_send(sample.to_f32()).unwrap();
                     }
                 }
                 cpal::StreamData::Input {
                     buffer: cpal::UnknownTypeInputBuffer::F32(buffer),
                 } => {
-                    if let Ok(mut guard) = writer_2.try_lock() {
-                        if let Some(writer) = guard.as_mut() {
-                            for &sample in buffer.iter() {
-                                writer.write_sample(sample).ok();
-                            }
-                        }
+                    for &sample in buffer.iter() {
+                        sender.try_send(sample).unwrap();
                     }
                 }
                 _ => (),
             }
         });
     });
-    Ok((recording, writer))
+    Ok(receiver)
 }
 
 fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
